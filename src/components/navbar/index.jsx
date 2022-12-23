0b39cdc312038ecc0a4bf4ba85c0c8735868aa93
Jadit19 <jadit19@gmail.com>
2022-12-23 00:11:23 +0000

    feature - change theme

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 import "./style.css";
 
 function Navbar() {
+    const [isDark, setIsDark] = useState(0);
 
-    const handleClick = () => {
+    const handleNavClick = () => {
         document.querySelector(".nav__button").classList.toggle("open");
         document.querySelector(".nav__overlay").classList.toggle("open")
+    };
+    
+    const handleThemeClick = () => {
+        if (isDark)
+            document.documentElement.className = "dark";
+        else
+            document.documentElement.className = "light";
+        setIsDark(!isDark);
     };
 
     return (
         <>
             <nav>
-                <div className="nav__button" onClick={handleClick}>
+                <div className="nav__button" onClick={handleNavClick}>
                     <span></span>
                     <span></span>
                     <span></span>
@@ -20,7 +31,11 @@ function Navbar() {
 
             </nav>
 
-            <div className="nav__overlay">
+            <div className="nav__overlay" onClick={handleThemeClick}>
+                <div className="theme__container">
+                    { isDark ? <DarkModeIcon /> : <LightModeIcon /> }
+                </div>
+
                 <div className="nav__container">
                     <a href="/">Home</a>
                     <a href="/">About Us</a>
